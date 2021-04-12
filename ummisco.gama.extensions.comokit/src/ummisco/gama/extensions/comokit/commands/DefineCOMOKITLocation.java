231f1953407fa090fa289d1b4f79297ccbf89ab6
ptaillandier <patrick.taillandier@gmail.com>
2021-04-12 13:51:51 +0700

    New plugin for COMOKIT

---
+++ ummisco.gama.extensions.comokit/src/ummisco/gama/extensions/comokit/commands/DefineCOMOKITLocation.java
@@ -0,0 +1,40 @@
+package ummisco.gama.extensions.comokit.commands;
+
+import java.io.File;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+import msi.gama.runtime.GAMA;
+import msi.gama.util.GamaListFactory;
+import msi.gama.util.IList;
+import msi.gama.util.IMap;
+import msi.gama.util.file.GamaFolderFile;
+import msi.gaml.operators.System;
+import msi.gaml.types.IType;
+import msi.gaml.types.Types;
+
+
+public class DefineCOMOKITLocation extends AbstractHandler{
+
+	static public String COMOKIT_PATH = GAMA.getModel().getWorkingPath();
+	
+		@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IList l = GamaListFactory.create();
+		l.add(System.enterValue(GAMA.getRuntimeScope(), "COMOKIT Folder", Types.get(IType.DIRECTORY), COMOKIT_PATH));
+		IMap<String,Object> result = System.userInput(GAMA.getRuntimeScope(), l);
+		if (result == null || (result.get("COMOKIT Folder") == null)) return null;
+		String path =((GamaFolderFile) result.get("COMOKIT Folder")).getPath(null);
+		if (path != null) {
+			File f = new File(path);
+			if (f.isDirectory()) {
+				COMOKIT_PATH = path;
+			}
+		}
+			
+		return null;
+	}
+}
---
+++ ummisco.gama.extensions.comokit/src/ummisco/gama/extensions/comokit/commands/GenerateGISEnvironment.java
@@ -0,0 +1,33 @@
+package ummisco.gama.extensions.comokit.commands;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.common.util.URI;
+
+import msi.gama.kernel.model.IModel;
+import msi.gama.lang.gaml.validation.GamlModelBuilder;
+import msi.gama.runtime.GAMA;
+import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gaml.compilation.GamlCompilationError;
+
+public class GenerateGISEnvironment extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		String path = DefineCOMOKITLocation.COMOKIT_PATH + "\\Utilities\\Generate GIS Data.gaml";
+		File f = new File(path);
+		if (!f.exists()) {
+			GamaRuntimeException.error(path + " not found, use the \"Locate COMOKIT\" item in the COMOKIT model to set the path to COMOKIT", null);
+		}
+		final List<GamlCompilationError> errors = new ArrayList<>();
+		final IModel model = GamlModelBuilder.getDefaultInstance().compile(URI.createFileURI(path), errors);
+		GAMA.runGuiExperiment("wizard_xp", model);
+		return null;
+	}
+
+}
